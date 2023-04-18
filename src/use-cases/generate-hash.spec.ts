6efaea2f34270e8936f9e6a58c1c1732a65db25a
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-04-18 19:32:22 -0300

    test: ensure UrlShortner.generate add in database

--- src/use-cases/generate-hash.spec.ts
+++ src/use-cases/generate-hash.spec.ts
@@ -1,22 +1,70 @@
-class UrlShortener {
-  constructor(private readonly repository: UrlRepository) {}
+interface UrlRepository {
+  add: (input: object) => Promise<string>
+  load: (url: string) => Promise<string | null>
 }
+interface Encoder {
+  encode: (url: string) => Promise<string>
+}
+class UrlShortener {
+  constructor(
+    private readonly repository: UrlRepository,
+    private readonly encoder: Encoder
+  ) {}
 
-interface UrlRepository {}
+  async generate(url: string) {
+    let hash = await this.repository.load(url)
+    if (hash) {
+      return hash
+    }
+    hash = await this.encoder.encode(url)
+    this.repository.add({ url, hash })
+    return hash
+  }
+}
 class UrlRepositorySpy implements UrlRepository {
   urlCount = 0
+  hash = ""
+
+  async load(url: string) {
+    return this.hash
+  }
+
+  async add(input: object) {
+    this.urlCount++
+    return this.hash
+  }
+}
+
+class EncoderSpy implements Encoder {
+  hash = ""
+  async encode(url: string) {
+    return this.hash
+  }
 }
 
 const makeSut = () => {
   const urlRepository = new UrlRepositorySpy()
-  const sut = new UrlShortener(urlRepository)
-  return { sut, urlRepository }
+  const encoder = new EncoderSpy()
+  const sut = new UrlShortener(urlRepository, encoder)
+  return { sut, urlRepository, encoder }
 }
 
 describe("UrlShortener", () => {
-  test("Should not add a hash in repository at initialization", async () => {
-    const { urlRepository } = makeSut()
+  describe("When create a instance", () => {
+    test("Should not add a hash in repository at initialization", async () => {
+      const { urlRepository } = makeSut()
 
-    expect(urlRepository.urlCount).toBe(0)
+      expect(urlRepository.urlCount).toBe(0)
+    })
+  })
+
+  describe("When generate a hash", () => {
+    test("Should add url and hash to db", async () => {
+      const { sut, urlRepository } = makeSut()
+
+      await sut.generate("any_url")
+
+      expect(urlRepository.urlCount).toBe(1)
+    })
   })
 })
