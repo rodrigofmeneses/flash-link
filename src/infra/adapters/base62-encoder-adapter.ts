96de32fb3711148687b9a1dcf0a65ed1c2cc4427
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-04-20 11:08:49 -0300

    refactor: move Base62EncoderAdapter to own file

---
+++ src/infra/adapters/base62-encoder-adapter.ts
@@ -0,0 +1,22 @@
+import { Encoder } from "../../utils/helpers/encoder"
+
+export class Base62EncoderAdapter implements Encoder {
+  CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  async encode(id: number) {
+    if (id < 0) {
+      throw new Error()
+    }
+
+    let value = id
+    let remainder: number
+    let shortUrl = ""
+    do {
+      remainder = value % 62
+      value = Math.floor(value / 62)
+      shortUrl = shortUrl + this.CHARACTERS[remainder]
+    } while (value != 0)
+
+    return shortUrl.split("").reverse().join("")
+  }
+}
