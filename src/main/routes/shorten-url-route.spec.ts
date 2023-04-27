83dd5653b96fc964ea05e7a133b68388172ffd1a
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-04-27 10:52:55 -0300

    test: ensure POST /api/v1/shorten with valid body returns 200

--- src/main/routes/shorten-url-route.spec.ts
+++ src/main/routes/shorten-url-route.spec.ts
@@ -1,7 +1,26 @@
+import { MongoMemoryServer } from "mongodb-memory-server"
 import { app } from "../config/app"
 import request from "supertest"
+import {
+  cleanDb,
+  inMemoryConnect,
+  inMemoryDisconnect,
+} from "../../infra/helpers/mongo-helper"
 
 describe("ShortenUrlRouter", () => {
+  let mongoServer: MongoMemoryServer
+  beforeAll(async () => {
+    mongoServer = await inMemoryConnect()
+  })
+
+  beforeEach(async () => {
+    await cleanDb()
+  })
+
+  afterAll(async () => {
+    await inMemoryDisconnect(mongoServer)
+  })
+
   describe("When Post in /api/v1/shorten", () => {
     describe("with no body", () => {
       test("should response status 400", async () => {
@@ -18,6 +37,14 @@ describe("ShortenUrlRouter", () => {
 
         await request(app).post("/api/v1/shorten").send(body).expect(400)
         await request(app).post("/api/v1/shorten").send(body2).expect(400)
+      })
+    })
+
+    describe("with valid body", () => {
+      test("should response status 200", async () => {
+        const body = { longUrl: "validUrl" }
+
+        await request(app).post("/api/v1/shorten").send(body).expect(200)
       })
     })
   })
