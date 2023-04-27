fbbadf5d5db675666685589b923b8a1fb9ad428f
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-04-27 11:28:12 -0300

    test: ensure GET /:shortUrl redirect correctly

--- src/main/routes/redirect-url-route.spec.ts
+++ src/main/routes/redirect-url-route.spec.ts
@@ -1,13 +1,45 @@
+import { MongoMemoryServer } from "mongodb-memory-server"
 import { app } from "../config/app"
 import request from "supertest"
+import {
+  cleanDb,
+  inMemoryConnect,
+  inMemoryDisconnect,
+} from "../../infra/helpers/mongo-helper"
 
 describe("ShortenUrlRouter", () => {
-  describe("When Get in /:shortUrl", () => {
+  let mongoServer: MongoMemoryServer
+  beforeAll(async () => {
+    mongoServer = await inMemoryConnect()
+  })
+
+  beforeEach(async () => {
+    await cleanDb()
+  })
+
+  afterAll(async () => {
+    await inMemoryDisconnect(mongoServer)
+  })
+  describe("When GET /:shortUrl", () => {
     describe("with incorrect shortUrl", () => {
       test("should response status 404", async () => {
         const param = "incorrectParam"
 
         await request(app).get(`/${param}`).expect(404)
+      })
+    })
+    describe("with correct shortUrl", () => {
+      test("should redirect status 301", async () => {
+        const longUrl = "valid_url"
+        const response = await request(app)
+          .post("/api/v1/shorten")
+          .send({ longUrl })
+        const { shortUrl } = response.body
+
+        await request(app)
+          .get(`/${shortUrl}`)
+          .expect("location", longUrl)
+          .expect(302)
       })
     })
   })
